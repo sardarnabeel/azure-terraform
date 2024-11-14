e8ab68f18220816f9a5dad90bf9998dab8ded4f4
nabeel <nabeel@gmail.com>
2024-11-14 00:47:52 -0800

    added

--- modules/wordpress_nsg/main.tf
+++ modules/wordpress_nsg/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "wordpress-nsg" {
-  name                = var.nsg_name
+  name                = var.wordpress-nsg-var.nsg_name
   location            = var.location
   resource_group_name = var.resource_group_name
 }
